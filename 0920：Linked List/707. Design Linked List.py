75a1069f25738c37b84be44c161a43cee04e477d
ChengShaoChi <55570332+ChengShaoChi@users.noreply.github.com>
2019-10-16 20:12:33 +0800

    Update 707. Design Linked List.py

--- 0920：Linked List/707. Design Linked List.py
+++ 0920：Linked List/707. Design Linked List.py
@@ -1,51 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 class MyLinkedList:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.linkedlist = []
 
+    #返回linkedlist中第index個節點的值。
+    #如果index無效（小於0或大於等於linkedlist的長度），則返回-1。
     def get(self, index: int) -> int:
-        """
-        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
-        """
         if index < 0 or index >= len(self.linkedlist):
             return -1
         else:
             return self.linkedlist[index]
 
+    #在linkedlist的第一個元素之前添加一個值節點。插入後，新節點將成為linkedlist的第一個節點。
     def addAtHead(self, val: int) -> None:
-        """
-        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-        """
         self.linkedlist.insert(0,val)
 
+    #將值節點添加到linkedlist的最後一個元素。
     def addAtTail(self, val: int) -> None:
-        """
-        Append a node of value val to the last element of the linked list.
-        """
         self.linkedlist.append(val)
 
+    #在linkedlist的第index個節點之前添加一個值節點。
+    #如果index等於linkedlist的長度，則添加到末端。
+    #如果index大於linkedlist的長度，則不會插入。
     def addAtIndex(self, index: int, val: int) -> None:
-        """
-        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
-        """
         if index < len(self.linkedlist):
             self.linkedlist.insert(index,val)
         elif index == len(self.linkedlist):
             self.linkedlist.append(val)
 
+    #如果index有效（大於等於0和小於linkedlist的長度），則刪除linkedlist中第index個節點。
     def deleteAtIndex(self, index: int) -> None:
-        """
-        Delete the index-th node in the linked list, if the index is valid.
-        """
         if index >= 0 and index < len(self.linkedlist):
             return self.linkedlist.pop(index)
 
