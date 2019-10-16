742c22f222ad5507b270651b09503cf3a9f09bfd
ChengShaoChi <55570332+ChengShaoChi@users.noreply.github.com>
2019-10-16 20:13:28 +0800

    Delete 206. Reverse Linked List.py

--- 0920：Linked List/206. Reverse Linked List.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        prev = None
-        while head:
-            current = head
-            head = head.next
-            current.next = prev
-            prev = current
-        return prev
