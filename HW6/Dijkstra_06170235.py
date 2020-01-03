1ceccf30445cb02d36ed7422cb059f906c70cc62
ChengShaoChi <55570332+ChengShaoChi@users.noreply.github.com>
2020-01-03 23:35:29 +0800

    Update Dijkstra_06170235.py

--- HW6/Dijkstra_06170235.py
+++ HW6/Dijkstra_06170235.py
@@ -9,6 +9,7 @@ class Graph():
         #創一個vertices乘vertices的矩陣，裡面都是0。column：有幾直／row：有幾橫。
         
     def addEdge(self,u,v,w):
+        self.graph.append([u,v,w]) 
         
     def minDistance(self, dist, found):#在還沒找到過的點中找到有最小距離的點
         unlm = sys.maxsize#令到還不能到的點的距離是無限大，這裡用Python中整數的最大值代替
