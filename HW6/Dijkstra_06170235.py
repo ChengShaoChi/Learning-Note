62fa091d449bd4f9e0a0477e20bc15bce19f52a5
ChengShaoChi <55570332+ChengShaoChi@users.noreply.github.com>
2020-01-03 23:47:33 +0800

    Update Dijkstra_06170235.py

--- HW6/Dijkstra_06170235.py
+++ HW6/Dijkstra_06170235.py
@@ -40,4 +40,49 @@ class Graph():
             SSSP[str(i)] = dist[i]
         return SSSP
       
+      
+      
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+      
+    def union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else : 
+            parent[yroot] = xroot 
+            rank[xroot] += 1
+
     def Kruskal(self):
+        result =[]
+        i = 0
+        e = 0
+        self.graph = sorted(self.graph,key = lambda item:item[2]) 
+        parent = [] ; rank = [] 
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+        while e < self.V -1:
+            u,v,w =  self.graph[i]
+            i = i + 1
+            x = self.find(parent, u)
+            y = self.find(parent ,v)
+            if x != y:
+                e = e + 1
+                result.append([u,v,w])
+                self.union(parent, rank, x, y)
+                
+        MST = {}
+        for u, v, weight in result:
+            MST[u, v] = weight
+        return MST
+      
+#參考資料：
+#https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
+#https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-algo-2/
